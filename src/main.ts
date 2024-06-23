17e1ecb60261e40214770c1865b75b9553847c60
mustafaemirhanyildiz <mustafaemirhanyildiz@hotmail.com>
2024-06-24 00:05:33 +0300

    feat: Add @nestjs/swagger plugin and swagger-ui-express dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Example API')
+    .setDescription('The example API description')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
